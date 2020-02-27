072cd32a4892f4f2f89aac48d33b182b91103de6
yangxindi2017 <694076236@qq.com>
2020-02-27 20:56:33 +0800

    htmlhint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,13 @@
 module.exports = function(grunt){
   grunt.initConfig({
-    less:{
-      compile:{
-        files:{'dist/compiled.css':'css/*.less'}
-      }
+    htmlhint:{
+      options:{
+        htmlhintrc:'./.htmlhintrc'
+    },
+    src:['*.html']
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.registerTask('default',['less:compile']);
+  grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.registerTask('default',['htmlhint']);
 }
